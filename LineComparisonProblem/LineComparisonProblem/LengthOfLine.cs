63202951c871bb656b0e5f1ece713f24936f3917
mohan <sahumohan338@gmail.com>
2022-07-28 00:59:41 -0700

    [mohan]:add To Display Welcome Line Comparison

---
+++ LineComparisonProblem/LineComparisonProblem/LengthOfLine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    public class LengthOfLine
+    {
+        public static void LengthLine()
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation");
+        }
+    }
+}
---
+++ LineComparisonProblem/LineComparisonProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparisonProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LengthOfLine.LengthLine();
+        }
+    }
+}
