1ad503aa6ec9abde7fd782333599a8ca45b05c6d
Mohan Sahu <68227271+mohan3388@users.noreply.github.com>
2022-07-28 22:05:09 -0700

    Update Program.cs

--- LineComparisonProblem/LineComparisonProblem/Program.cs
+++ LineComparisonProblem/LineComparisonProblem/Program.cs
@@ -1,11 +1,11 @@
-﻿namespace LineComparisonProblem
+namespace LineComparisonProblem
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            LengthOfLine length = new LengthOfLine(1, 2, 1, 2);
-            length.calculateLength();
+            LengthOfLine length = new LengthOfLine();
+            length.Cartesian();
         }
     }
 }
